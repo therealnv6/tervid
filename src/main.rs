a7a4dbc4618a1150091c43fa27eed9538a94e44f
Patrick <96917554+patrickzondervan@users.noreply.github.com>
2022-09-09 13:18:16 +0200

    innit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use crossterm::{
+    cursor,
+    style::{self, Stylize},
+    terminal, ExecutableCommand, QueueableCommand, Result,
+};
+use std::{io::{stdout, Write}, thread};
+
+fn main() -> Result<()> {
+    let mut stdout = stdout();
+
+    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+
+    let mut current = 0;
+
+    loop {
+        current += 1;
+
+        let abs = match current % 2 {
+            0 => "█".magenta(),
+            _ => "█".blue(),
+        };
+
+        for y in 0..40 {
+            for x in 0..150 {
+                if (y == 0 || y == 40 - 1) || (x == 0 || x == 150 - 1) {
+                    stdout
+                        .queue(cursor::MoveTo(x, y))?
+                        .queue(style::PrintStyledContent(abs))?;
+                }
+            }
+        }
+        stdout.flush()?;  
+        thread::sleep_ms(25);
+    }
+
+    Ok(())
+}
